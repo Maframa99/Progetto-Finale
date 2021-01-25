97c2c5cbe86a41cc9985331e4b1b54a48b2e80fe
Kreaskan <75830067+Kreaskan@users.noreply.github.com>
2021-01-25 13:15:37 +0100

    Script che ti ha fatto smadonnare
    "non mi piace il methodgroup, GNE GNE" - codice dimmerda

---
+++ Music_Select.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Music_Select : MonoBehaviour
+{
+    
+	public GameObject Label; 
+	
+	public GameObject[] MusicList;
+	
+	private GameObject Selected_Music;
+	
+	public void handleinputdata ()
+	{
+		
+		Text Testo = Label.GetComponent<Text>();
+		
+		/*foreach(GameObject child in MusicList)
+		{
+			if(child[==Testo.ToString()]
+			
+			{
+				
+				Selected_Music=child;
+			}
+			
+		}*/
+		
+		for (int i=0; i<MusicList.Length;i++)
+		{
+			if(MusicList[i].name==Testo.ToString())
+			{
+				Selected_Music=MusicList[i];
+			}	
+		}
+		Debug.Log (Selected_Music);
+	}
+
+}
